2fdc8ffa539a31e9f592ca15794dda584d3f148c
Angga <anggabagussusilo8@gmail.com>
2024-11-16 10:36:35 +0700

    sort-product

--- Modules/Shop/Http/Controllers/ProductController.php
+++ Modules/Shop/Http/Controllers/ProductController.php
@@ -14,6 +14,7 @@ class ProductController extends Controller
     protected $productRepository;
     protected $categoryRepository;
     protected $tagRepository;
+    protected $defaultPriceRange;
 
     public function __construct(ProductRepositoryInterface $productRepository, CategoryRepositoryInterface $categoryRepository, TagRepositoryInterface $tagRepository)
     {
@@ -22,19 +23,35 @@ class ProductController extends Controller
         $this->productRepository = $productRepository;
         $this->categoryRepository = $categoryRepository;
         $this->tagRepository = $tagRepository;
+        $this->defaultPriceRange = [
+            'min' => 10000,
+            'max' => 75000,
+        ];
 
         $this->data['categories'] = $this->categoryRepository->findAll();
+        $this->data['filter']['price'] = $this->defaultPriceRange;
     }
     /**
      * Display a listing of the resource.
      * @return Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
+        $priceFilter = $this->getPriceRangeFilter($request);
+
         $options = [
             'per_page' => $this->perPage,
+            'filter' => [
+                'price' => $priceFilter,
+            ],
         ];
+
+        if ($request->get('price')) {
+            $this->data['filter']['price'] = $priceFilter;
+        }
+        
         $this->data['products'] = $this->productRepository->findAll($options);
+        
         return $this->loadTheme('products.index', $this->data);
     }
     public function category($categorySlug)
@@ -67,5 +84,21 @@ class ProductController extends Controller
         $this->data['tag'] = $tag;
 
         return $this->loadTheme('products.tag', $this->data);
+    }
+    function getPriceRangeFilter($request)
+    {
+        if (!$request->get('price')) {
+            return [];
+        }
+
+        $prices = explode(' - ', $request->get('price'));
+        if (count($prices) < 0) {
+            return $this->defaultPriceRange;
+        }
+
+        return [
+            'min' => (int) $prices[0],
+            'max' => (int) $prices[1],
+        ];
     }
 }
